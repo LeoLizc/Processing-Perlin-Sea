ba5836ff1b80991c4a9d51e2440111bfdc8b5b6f
LeoLizc <ldlizcano@uninorte.edu.co>
2023-07-08 22:05:52 -0500

    Mesh moved to the PerlinSea class

--- src/com/leolizc/perlinSea/PerlinSea.java
+++ src/com/leolizc/perlinSea/PerlinSea.java
@@ -5,8 +5,26 @@ import processing.core.PApplet;
 public class PerlinSea {
     PApplet p;
 
-    public PerlinSea(PApplet p){
+    public PerlinSea(PApplet p) {
         this.p = p;
     }
 
+    public void render() {
+        // translate(width/2f, height/2f, 0);
+        p.rotateX(p.PI / 3f);
+        p.rotateZ(p.frameCount * p.PI / 180f * 0.4f);
+        p.noFill();
+
+        // 20x20 squares From x = -200 to 200, draw a line from y = -200 to 200
+        p.beginShape();
+        p.vertex(-200, 200, 0);
+        for (int y = 200; y > -200; y -= 20) {
+            for (int x = 200; x >= -200; x -= 20) {
+                p.vertex(x, y, 0);
+                p.vertex(x, y - 20, 0);
+            }
+        }
+        p.vertex(200, -200, 0);
+        p.endShape();
+    }
 }
